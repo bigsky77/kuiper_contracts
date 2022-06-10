076781433db66c601f5dac13b0c052aafc781716
bigsky77 <simonjudd2@gmail.com>
2022-06-09 20:00:58 -0700

    update

--- contracts/test/Contract.t.sol
+++ contracts/test/Contract.t.sol
@@ -2,6 +2,10 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
+import "../src/Basket.sol";
+import "../src/Auction.sol";
+import "../src/Factory.sol";
+
 
 contract ContractTest is Test {
     function setUp() public {}
