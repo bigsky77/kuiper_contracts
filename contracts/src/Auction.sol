62c3167cde3abb151be043b8e0fd7a2b673a7bb6
bigsky77 <simonjudd2@gmail.com>
2022-06-09 20:07:26 -0700

    update

--- contracts/src/Auction.sol
+++ contracts/src/Auction.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity =0.8.7;
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { SafeERC20 } from "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { ReentrancyGuard } from "openzeppelin/contracts/security/ReentrancyGuard.sol";
 import './interfaces/IFactory.sol';
 import './interfaces/IBasket.sol';
 import "./interfaces/IAuction.sol";
--- contracts/src/Factory.sol
+++ contracts/src/Factory.sol
@@ -1,11 +1,11 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity =0.8.7;
 
-import { SafeERC20 } from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
-import { IERC20 } from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
-import { Clones } from "@openzeppelin/contracts/proxy/Clones.sol";
-import { ReentrancyGuard } from "@openzeppelin/contracts/security/ReentrancyGuard.sol";
+import { SafeERC20 } from "openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import { IERC20 } from "openzeppelin/contracts/token/ERC20/IERC20.sol";
+import { Ownable } from "openzeppelin/contracts/access/Ownable.sol";
+import { Clones } from "openzeppelin/contracts/proxy/Clones.sol";
+import { ReentrancyGuard } from "openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./interfaces/IAuction.sol";
 import "./interfaces/IBasket.sol";
 import "./interfaces/IFactory.sol";
